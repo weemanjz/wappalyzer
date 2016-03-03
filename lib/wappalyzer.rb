5f46f7940c5adf027051c85a4ed95090f6f02722
pulkit21 <pulkit.ag02@gmail.com>
2016-03-03 16:02:06 +0530

    Added the Config to detect the server side language used for the website

--- lib/wappalyzer.rb
+++ lib/wappalyzer.rb
@@ -1,5 +1,45 @@
+#!/usr/bin/env ruby
+
 require "wappalyzer/version"
 
+require 'net/http'
+require 'v8'
+require 'json'
+
+Encoding.default_external = Encoding::UTF_8
+
 module Wappalyzer
-  # Your code goes here...
+  class Detector
+    def initialize
+      @realdir = File.dirname(File.realpath(__FILE__))
+      file = File.join(@realdir, 'apps.json')
+      @json = JSON.parse(IO.read(file))
+      @categories, @apps = @json['categories'], @json['apps']
+    end
+
+    def analyze(url)
+      uri, body, headers = URI(url), nil, {}
+      Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :open_timeout => 5) do |http|
+        resp = http.get(uri.request_uri)
+        resp.each_header{|k,v| headers[k.downcase] = v}
+        body = resp.body.encode('UTF-8', :invalid => :replace, :undef => :replace)
+      end
+
+      cxt = V8::Context.new
+      cxt.load File.join(@realdir, 'js', 'wappalyzer.js')
+      cxt.load File.join(@realdir, 'js', 'driver.js')
+      data = {'host' => uri.hostname, 'url' => url, 'html' => body, 'headers' => headers}
+      output = cxt.eval("w.apps = #{@apps.to_json}; w.categories = #{@categories.to_json}; w.driver.data = #{data.to_json}; w.driver.init();")
+      JSON.load(output)
+    end
+  end
+end
+
+if $0 == __FILE__
+  url = ARGV[0]
+  if url
+    puts JSON.pretty_generate(Wappalyzer::Detector.new.analyze(ARGV[0]))
+  else
+    puts "Usage: #{__FILE__} http://example.com"
+  end
 end
--- wappalyzer.gemspec
+++ wappalyzer.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["pulkit21"]
   spec.email         = ["pulkit.ag02@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Analyzes a provided url and returns any services it can detect}
+  spec.description   = %q{This is merely a port of the javascript parts of Wappalyzer extension for Firefox and Chrome. It analyzes a url and tries to guess what software it uses (like server software, CMS, framework, programming language).}
+  spec.homepage      = "https://github.com/pulkit21/wappalyzer"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
   # delete this section to allow pushing this gem to any host.
@@ -29,4 +29,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "therubyracer", "~> 0.12.2"
 end

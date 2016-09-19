ff8c73064371ed7d42635a8507ca280273ed92e9
Piotr Brzózka <weemanjz@gmail.com>
2016-09-19 08:48:05 +0200

    use poltergeist for better tools detection

--- lib/wappalyzer.rb
+++ lib/wappalyzer.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
-require "wappalyzer/version"
+require 'wappalyzer/version'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
 
 require 'net/http'
 require 'v8'
@@ -10,27 +12,77 @@ Encoding.default_external = Encoding::UTF_8
 
 module Wappalyzer
   class Detector
+    include Capybara::DSL
+
     def initialize
       @realdir = File.dirname(File.realpath(__FILE__))
       file = File.join(@realdir, 'apps.json')
       @json = JSON.parse(IO.read(file))
-      @categories, @apps = @json['categories'], @json['apps']
+      @categories = @json['categories']
+      @apps = @json['apps']
+      configure_capybara
+      configure_driver
     end
 
     def analyze(url)
-      uri, body, headers = URI(url), nil, {}
-      Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :open_timeout => 5) do |http|
-        resp = http.get(uri.request_uri)
-        resp.each_header{|k,v| headers[k.downcase] = v}
-        body = resp.body.encode('UTF-8', :invalid => :replace, :undef => :replace)
-      end
+      uri = URI(url)
+      Capybara.reset_sessions!
+      visit url
 
-      cxt = V8::Context.new
-      cxt.load File.join(@realdir, 'js', 'wappalyzer.js')
-      cxt.load File.join(@realdir, 'js', 'driver.js')
-      data = {'host' => uri.hostname, 'url' => url, 'html' => body, 'headers' => headers}
-      output = cxt.eval("w.apps = #{@apps.to_json}; w.categories = #{@categories.to_json}; w.driver.data = #{data.to_json}; w.driver.init();")
+      data = { 'host' => uri.hostname, 'url' => url,
+               'html' => page.html, 'headers' => response_headers }
+      output = evaluate_output(data)
       JSON.load(output)
+    end
+
+    private
+
+    def context
+      @cxt ||= begin
+        cxt = V8::Context.new
+        cxt.load File.join(@realdir, 'js', 'wappalyzer.js')
+        cxt.load File.join(@realdir, 'js', 'driver.js')
+        cxt
+      end
+    end
+
+    def response_headers
+      headers = {}
+      page.response_headers.each { |k, v| headers[k.downcase] = v }
+      headers
+    end
+
+    def evaluate_output(data)
+      context.eval("w.apps = #{@apps.to_json}; "\
+                   "w.categories = #{@categories.to_json};"\
+                   "w.driver.data = #{data.to_json};"\
+                   'w.driver.init();')
+    end
+
+    def user_agent
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) '\
+      'Chrome/26.0.1410.43 Safari/537.31'
+    end
+
+    def configure_capybara
+      Capybara.run_server = false
+      Capybara.register_driver :poltergeist do |app|
+        Capybara::Poltergeist::Driver.new(
+          app,
+          # Raise JavaScript errors to Ruby
+          js_errors: false,
+          # Additional command line options for PhantomJS
+          phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=any']
+        )
+      end
+      Capybara.current_driver = :poltergeist
+    end
+
+    def configure_driver
+      page.driver.resize(1024, 768)
+      page.driver.headers = {
+        'User-Agent' => user_agent
+      }
     end
   end
 end
--- wappalyzer.gemspec
+++ wappalyzer.gemspec
@@ -13,15 +13,15 @@ Gem::Specification.new do |spec|
   spec.description   = %q{This analyzes a url and tries to guess what software it uses (like server software, CMS, framework, programming language).}
   spec.homepage      = "https://github.com/pulkit21/wappalyzer"
 
-
-
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
   spec.add_dependency "bundler", "~> 1.11"
-  spec.add_dependency "rake", "~> 10.0"
+  spec.add_dependency "rake", "~> 11.2.2"
   spec.add_dependency "rspec", "~> 3.0"
   spec.add_dependency "therubyracer", "~> 0.12.2"
+  spec.add_dependency 'poltergeist', '~> 1.8.1'
+  spec.add_dependency 'capybara', '~> 2.8.1'
 end
